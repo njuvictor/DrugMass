ce04826fcd5d6f80767b956977361cf96c9dc175
ajing <ajingnk@gmail.com>
2015-05-05 10:47:28 -0400

    add rentetion time variable

--- DrugMass.py
+++ DrugMass.py
@@ -109,10 +109,11 @@ def main():
     ms_file = tkFileDialog.askopenfilename()
     #ms_file   = "./Data/CCG224144MIDSample5minMS2.mzML"
     #ms_file   = "./Data/CCG224144MIDSample5min.mzML"
-    exspec = ExtractSpec(ms_file)
     mass_list = [423.3, 268.2]
+    retention_time = [0, 30]
+    exspec = ExtractSpec(ms_file)
     run = pymzml.run.Reader(ms_file, noiseThreshold = 100)
-    SlidingWindow(mass_list, run, exspec)
+    SlidingWindow(mass_list, run, exspec, retention_time)
 
 if __name__ == "__main__":
     main()
