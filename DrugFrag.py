a9e653f1c1f449cc5c811d425804d0df94e5a87e
ajing <ajingnk@gmail.com>
2015-05-13 17:26:38 -0400

    update for sum intensity

--- DrugFrag.py
+++ DrugFrag.py
@@ -2,6 +2,11 @@
     Get drug fragment mass
 '''
 
+from ExtractChrom import ExtractSpec
+from DrugMass import HighestPeaks, SelectPeaks
+import pymzml
+import Tkinter, tkFileDialog
+
 def DrugFragMass(masslist, i):
     '''
         The function sort masslist first and then add mass for each element before index i
@@ -13,6 +18,50 @@ def DrugFragMass(masslist, i):
         mass_list_mod[j] = mass_list_mod[j] + 16
     return mass_list_mod
 
-if __name__ == "__main__":
+def GetSumIntensityInOneSpec(mz_list, one_spec, tolerance = 0.2):
+    '''
+        Get the sum intensity for one spectrum for a list of mz in a collection of specs
+    '''
+    print_list = []
+    max_int_dict = dict()
+    spec = one_spec.spec
+    for eachmz in mz_list:
+        eachrange = (eachmz - tolerance, eachmz + tolerance)
+        try:
+            mz, intensity = HighestPeaks(SelectPeaks(spec["peaks"], eachrange))
+        except Exception as e:
+            #print e.message
+            continue
+        max_int_dict[eachmz] = {"max_int": intensity, "max_mz": mz, "max_time": spec["scan time"], "max_id": spec["id"]}
+    sum_intensity = sum([value["max_int"] for key, value in max_int_dict.iteritems()])
+    return sum_intensity
+
+def SpecSumIntensity4MassList(mzlist, rt_time, exspec):
+    specs = exspec.extractWithTime(rt_time)
+    if not specs:
+        # if specs empty
+        tol   = 0.01
+        specs = exspec.extractWithTimeRange(rt_time - tol, rt_time + tol)
+        spec  = specs[len(specs) / 2]
+    else:
+        spec  = specs[0]
+    return GetSumIntensityInOneSpec(mzlist, spec)
+
+def main():
+    root = Tkinter.Tk()
+    root.withdraw()
+    #ms_file = tkFileDialog.askopenfilename()
+    ms_file   = "./Data/CCG224144MIDSample5minMS2.mzML"
     mass_list = [439, 421, 312.2, 252, 170.8]
-    print DrugFragMass(mass_list, 2)
+    exspec = ExtractSpec(ms_file)
+    run = pymzml.run.Reader(ms_file, noiseThreshold = 100)
+    intensity_1 = SpecSumIntensity4MassList(mass_list, 5.83, exspec)
+    print 5.83, intensity_1
+    intensity_1 = SpecSumIntensity4MassList(mass_list, 5.681, exspec)
+    print 5.681, intensity_1
+
+if __name__ == "__main__":
+    #mass_list = [439, 421, 312.2, 252, 170.8]
+    #print DrugFragMass(mass_list, 2)
+    main()
+
